16f55fd63bc03018087cc1617031f3c872ac0319
Justin Keenan <jak244@pitt.edu>
2017-10-23 13:38:55 -0400

    Starting Ex 3

---
+++ src/java/Exercise3.java
@@ -0,0 +1,21 @@
+public class Exercise3 {
+
+  public static void main(String[] args) {
+
+    if(args.length == 1) {
+
+    }
+    else {
+      System.out.println("You have the incorrect number of arguments");
+      System.exit(1);
+    }
+  }
+
+  public static void triNum(int num) {
+
+  }
+
+  public static void lcNum(int num) {
+
+  }
+}
